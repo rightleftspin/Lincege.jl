5f09d7e4fbc267e208ef8102cdfa7c173c6b830f
Pranav <pranav@myrdd.info>
2026-06-16 14:43:37 -0400

    Adding basic physics solver code for simple thermodynamics. A reference implementattion for the usage of NLCE via LINCEGE.jl.

---
+++ src/Importers/Importers.jl
@@ -0,0 +1,57 @@
+include("ImportedCluster.jl")
+
+"""
+    ImportedCluster
+
+A single cluster deserialized from a JSON file produced by `write_to_json`. To be used in subsequent physics algorithms
+"""
+struct ImportedCluster
+        cluster_hash::UInt64
+        order::Int
+        n_sites::Int
+        coordinates::Vector{Vector{Float64}}
+        site_colors::Vector{Int}
+        bonds::Vector{Tuple{Int,Int,Int}}
+        weights::Vector{Float64}
+end
+
+"""
+    import_from_json(filepath) -> Vector{ImportedCluster}
+
+Read a JSON file produced by `write_to_json` and return a vector of
+`ImportedCluster`, one per cluster entry in the file.
+"""
+function import_from_json(filepath::String)::Vector{ImportedCluster}
+        data = JSON.parsefile(filepath)
+        clusters = Vector{ImportedCluster}(undef, length(data))
+
+        for (i, d) in enumerate(data)
+                cluster_hash = parse(UInt64, d["cluster_hash"])
+                order = d["order"]::Int
+                n_sites = d["n_sites"]::Int
+                coordinates = [Float64.(coord) for coord in d["coordinates"]]
+                site_colors = Int.(d["site_colors"])
+                bonds = Tuple{Int,Int,Int}[(b[1], b[2], b[3]) for b in d["bonds"]]
+                weights = Float64.(d["weights"])
+
+                clusters[i] = ImportedCluster(cluster_hash, order, n_sites, coordinates, site_colors, bonds, weights)
+        end
+
+        clusters
+end
+
+"""
+    import_from_json_by_order(filepath) -> Vector{Vector{ImportedCluster}}
+
+Like `import_from_json`, but returns clusters grouped by order. The returned
+vector is indexed by order: `result[order]` holds all clusters of that order.
+"""
+function import_from_json_by_order(filepath::String)::Vector{Vector{ImportedCluster}}
+        clusters = import_from_json(filepath)
+        max_order = maximum(c.order for c in clusters)
+        by_order = [Vector{ImportedCluster}() for _ in 1:max_order]
+        for c in clusters
+                push!(by_order[c.order], c)
+        end
+        by_order
+end
--- src/Lincege.jl
+++ src/Lincege.jl
@@ -32,16 +32,35 @@ include("Lattices/Lattices.jl")
 include("Hashers/Hashers.jl")
 include("Clusters/Clusters.jl")
 include("Expansions/Expansions.jl")
+include("Importers/Importers.jl")
+include("Physics/Physics.jl")
 
-export AbstractVertices, LatticeVertices, ExpansionVertices,
-        Bond, UnitCell, ExpansionBond, ExpansionUnitCell, image_unit_cell,
-        SiteExpansionLattice, StrongClusterExpansionLattice, WeakClusterExpansionLattice,
-        TranslationClusterSet, IsomorphicClusterSet, SymmetricClusterSet,
-        clusters_from_lattice!, clusters_from_clusters!,
-        Expansion, summation!, write_to_json,
+# Vertices
+export AbstractVertices, LatticeVertices, ExpansionVertices
+
+# Unit Cells
+export Bond, UnitCell, ExpansionBond, ExpansionUnitCell, image_unit_cell
+
+# Lattices
+export SiteExpansionLattice, StrongClusterExpansionLattice, WeakClusterExpansionLattice
+
+# Hashers
+export TranslationHasher, IsomorphicHasher, SymmetricHasher
+
+# Clusters
+export TranslationClusterSet, IsomorphicClusterSet, SymmetricClusterSet,
+        clusters_from_lattice!, clusters_from_clusters!
+
+# Expansions
+export Expansion, summation!, write_to_json,
         print_latex_table, print_html_table, print_ascii_table,
-        TranslationHasher, IsomorphicHasher, SymmetricHasher, latex_table_column_labels
+        latex_table_column_labels
 
-# Extra Physics Related Code, generally slow and not needed for basic Cluster Expansion construction
-#include("Physics/Physics.jl")
+# Importers
+export ImportedCluster, import_from_json, import_from_json_by_order
+
+# Physics
+export AbstractPhysicsSolver, observables, eigenvalues, eigenvectors, cluster_weights,
+        IsingSolver,
+        NLCEResult, perform_nlce
 end
